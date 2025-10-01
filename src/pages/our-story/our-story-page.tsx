4644e889bff55236094933cd518a3152e9a6430b
Kevin Chang <kc1998dp@gmail.com>
2025-09-30 21:16:25 -0400

    nav button css updates; left-align our story page

--- src/pages/our-story/our-story-page.tsx
+++ src/pages/our-story/our-story-page.tsx
@@ -10,7 +10,7 @@ interface OurStoryPageProps {
 const OurStoryPage = ({ onClick }: OurStoryPageProps): React.ReactNode => {
   return (
     <div className="our-story-container">
-      <div className="headline-section" style={{ textAlign: 'center' }}>
+      <div className="headline-section">
         <span className="poppins-black our-story-headline" style={{ color: 'rgb(1, 63, 50)' }}>
           We Launched at $29/Month... <br />
           Then Realized We Were Still Part of the Problem...
